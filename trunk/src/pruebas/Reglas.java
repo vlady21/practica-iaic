873fb17aa988f4126817294a212633e5f282268c
jomy.mc <jomy.mc@cebbd470-ee21-11dd-ba34-c503179d78c3>
2009-05-03 22:54:49 +0000

    reglas y pruebas

--- trunk/src/pruebas/Reglas.java
+++ trunk/src/pruebas/Reglas.java
@@ -21,16 +21,44 @@ public class Reglas {
 		
 		//metemos los hechos, serian los datos del usuario
 		Fact f = new Fact("datos", rete);
-	      f.setSlotValue("estado_actual", new Value("busqueda", RU.STRING));
-	      f.setSlotValue("tipo_estudios", new Value("universitarios", RU.STRING));
+		/*
+		 * PREGUNTA CURRICULUM
+		 * */
+		  f.setSlotValue("estado_actual", new Value("busqueda", RU.STRING));
+	      f.setSlotValue("tipo_estudios", new Value("no_universitarios", RU.STRING));
 	      f.setSlotValue("conocimiento", new Value("investigacion", RU.STRING));
 	      f.setSlotValue("tipo_contrato", new Value("beca", RU.STRING));
+	      
+	      f.setSlotValue("desea_informacion", new Value("si", RU.STRING));
+	      f.setSlotValue("lee_prensa", new Value("si", RU.STRING));
 	      f.setSlotValue("ruta_fichero_salida", new Value(ruta, RU.STRING));
+	     /**/
+		
+		/*
+		 * PREGUNTA CURRICULUM
+		 * 
+			f.setSlotValue("estado_actual", new Value("curriculum", RU.STRING));
+			f.setSlotValue("tipo_estudios", new Value("universitarios", RU.STRING));
+			f.setSlotValue("experiencia", new Value("no", RU.STRING));
+			f.setSlotValue("numero_paginas_CV", new Value(2, RU.INTEGER));
+			f.setSlotValue("ruta_fichero_salida", new Value(ruta, RU.STRING));
+		*/
+		
+		/*
+		 * PREGUNTA CONTRATO
+		 * 
+			f.setSlotValue("estado_actual", new Value("contrato", RU.STRING));
+			f.setSlotValue("estudia", new Value("si", RU.STRING));
+			f.setSlotValue("conocimiento", new Value("basico", RU.STRING));
+			f.setSlotValue("tiempo_libre", new Value(8, RU.INTEGER));
+			f.setSlotValue("ruta_fichero_salida", new Value(ruta, RU.STRING));
+		*/
+		
 	      rete.assertFact(f);
 
 	      //mostramos los hechos y las reglas
 	      rete.executeCommand("(facts)");
-	      rete.executeCommand("(rules)");
+	      //rete.executeCommand("(rules)");
 	      
 	      //ejecutamos jess
 	      rete.executeCommand("(run)");
